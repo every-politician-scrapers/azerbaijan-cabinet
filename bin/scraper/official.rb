58c50c3079ec0b822581f9904eae2065fd828142
Tony Bowden <tony@tmtm.com>
2021-07-29 21:15:26 +0300

    Update to every-politician-scraper 0.0.21

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/scraper_data'
-require 'open-uri/cached'
 require 'pry'
 
 class MemberList
@@ -39,5 +38,5 @@ class MemberList
   end
 end
 
-url = 'https://nk.gov.az/en/page/56/'
-puts EveryPoliticianScraper::ScraperData.new(url).csv
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
