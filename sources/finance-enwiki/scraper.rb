571a67eb42f6b04879fbf5d9366584897578fce7
Tony Bowden <tony@tmtm.com>
2022-01-21 15:02:56 +0200

    compare enwiki Finance Ministers

---
+++ sources/finance-enwiki/scraper.rb
@@ -0,0 +1,39 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  # decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Finance Ministers'
+  end
+
+  # TODO: make this easier to override
+  def holder_entries
+    noko.xpath("//h3[.//span[contains(.,'#{header_column}')]][last()]//following-sibling::ul[1]//li[a]")
+  end
+
+  class Officeholder < OfficeholderBase
+    def combo_date?
+      true
+    end
+
+    def raw_combo_dates
+      noko.text.split(',', 2).last.split('-').map(&:tidy)
+    end
+
+    def name_cell
+      noko.css('a')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
